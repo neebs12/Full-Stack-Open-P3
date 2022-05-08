d32e3d5beb4d8e0df93af79cde2e4ab4f54890d7
Jason Aricheta <jason.aricheta@gmail.com>
2022-05-08 17:50:24 +1200

    added

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-require('dotenv').config();
-const morgan = require('morgan');
-const cors = require('cors');
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const morgan = require('morgan')
+const cors = require('cors')
+const express = require('express')
+const app = express()
 
-const Phonebook = require('./models/phonebook');
+const Phonebook = require('./models/phonebook')
 
 morgan.token('body', (req, res) => {
   if (Object.entries(req.body).length !== 0) 
-    return JSON.stringify(req.body);
-});
+    return JSON.stringify(req.body)
+})
 
 // cors should not be necessary
 // app.use(cors());
 app.use(express.static('build'))
-app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response, next) => {
   Phonebook.find({})
     .then(result => {
-      response.json(result);
+      response.json(result)
     })
-    .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -30,21 +30,21 @@ app.get('/info', (request, response) => {
     .then( result => {
       response.send(`<p>Phonebook has info for ${result.length} people </p><p>${new Date()}</p>`)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
   // based on id param, get the assc. object -> json
   Phonebook.findById(request.params.id)
     .then(result => {
-      response.status(200).json(result);
+      response.status(200).json(result)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response, next) => {
   // for a response to go through,
-  let newPerson = request.body;
+  let newPerson = request.body
 
   // if newPerson does not have "name" or "number"
   // -- the validation is deferred to the application-level
@@ -69,16 +69,16 @@ app.post('/api/persons', (request, response, next) => {
       // works to NOT display result as HTTP Code: 204 refers to No Content
       // response.status(204).json(result); 
       // works TO display result as HTTP Code: Created
-      response.status(201).json(result); // works to display result
+      response.status(201).json(result) // works to display result
       // Lesson: 
       // HTTP Codes are RESTful too, just get the right codes
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
   // update the information with PUT
-  let {name, number} = request.body;
+  let {name, number} = request.body
 
   // ok so this is weird, so this does not work to validate 
   // -- with missing number property. Even if that property is 
@@ -97,53 +97,53 @@ app.put('/api/persons/:id', (request, response, next) => {
     .then(result => {
       response.json(result)
     })
-    .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Phonebook.findByIdAndDelete(request.params.id)
     .then(_ => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint' });
+  response.status(404).send({error: 'unknown endpoint' })
 }
 
 // endpoint not found - invalid route
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 // error handler
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   // CastError is the error created with invalid ids
-  if (error.name === "CastError") {
+  if (error.name === 'CastError') {
     response.status(400).send({error: 'malformatted id'})
-    return;
-  } else if (error.name === "ValidationError") {
+    return
+  } else if (error.name === 'ValidationError') {
     // notice that this is .json and dependent on error.message,
     // this is because validation is (within) the schema of the database
     // it will auto-reject the update 
 
     // Also, due to the nature of our api, this should only be triggered by out post and put
     response.status(400).json({error: error.message})
-    return;
+    return
   }
 
   // note, this handler is not the final FINAL middleware
   // invocation of next should ideally be:
   // 1. Express' default error handler OR
   // 2. Another user-defined error handler middleware (thought)
-  next(error);
+  next(error)
 }
 
 // error handling
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Script Log: Listening on port: ${PORT}`);
-});
+  console.log(`Script Log: Listening on port: ${PORT}`)
+})
--- models/phonebook.js
+++ models/phonebook.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 console.log(`connecting to ${url}`)
 
 // start connection
 mongoose.connect(url)
   .then(result => {
-    console.log('connected to MongoDB');
+    console.log('connected to MongoDB')
   })
   .catch(error => {
-    console.log(`error connecting to MongoDB: ${error.message}`);
+    console.log(`error connecting to MongoDB: ${error.message}`)
   })
 
 // name and number are required
@@ -28,19 +28,19 @@ const phoneBookSchema = new mongoose.Schema({
     validate: {
       validator: numStr => {
         // by this point, numStr will have a length of 8 or more
-        let pt = numStr.split('-');
+        let pt = numStr.split('-')
         // 12345678 is a valid phone number
         // -- ie no second part on split, thus true
-        if (pt[1] === undefined) return true;
+        if (pt[1] === undefined) return true
 
         // cannot have pt1-pt2-pt3-...ptn
-        if (pt.length >= 3) return false;
+        if (pt.length >= 3) return false
 
         // if there is a second part, scrutinize length of first section
         // -- ok for 2 and 3
-        if (2 <= pt[0].length && pt[0].length <= 3) return true; 
+        if (2 <= pt[0].length && pt[0].length <= 3) return true 
 
-        return false;
+        return false
       }, 
       message: error => {
         return `${error.value} is not a valid number!`
@@ -54,12 +54,12 @@ const phoneBookSchema = new mongoose.Schema({
 // -- unsafe ._id object
 phoneBookSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
 })
 
-let model = mongoose.model('number', phoneBookSchema);
+let model = mongoose.model('number', phoneBookSchema)
 
-module.exports = model;
+module.exports = model
